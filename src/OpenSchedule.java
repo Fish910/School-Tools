57d06b7ad41263b5cb67bb15b1eec1aa25119913
Fish910 <liamkdowning910@gmail.com>
2025-05-05 11:47:11 -0400

    tools for school

--- src/OpenSchedule.java
+++ src/OpenSchedule.java
@@ -8,7 +8,7 @@ public class OpenSchedule
     private static Subject spanish3 = new Subject("Spanish 3", 2, "blue");
     private static Subject windEnsemble = new Subject("Wind Ensemble", 1, "green");
     private static Subject physics = new Subject("AP Physics", 1, "green");
-    private static Subject precalca = new Subject("Pre Calc/Calc A", 3, "green");
+    private static Subject precalcA = new Subject("Pre Calc/Calc A", 3, "green");
     private static Subject bio = new Subject("AP Bio", 1, "green");
     private static Subject forensics = new Subject("AP Forensics", 2, "blue");
     private static Subject psych = new Subject("AP Psych", 3, "blue");
@@ -27,11 +27,12 @@ public class OpenSchedule
         for (Block b : blocks) message += (b + " ");
         message += "\n";        
 
-        Student me = new Student("Liam", apush, csa, lang, spanish3, windEnsemble, physics, precalca);
+        Student me = new Student("Liam", apush, csa, lang, spanish3, windEnsemble, physics, precalcA);
         Student[] classmates = {
-            new Student("Brynn", apush, bio, lang, forensics, windEnsemble, apSpanish, precalca),
-            new Student("Tyler", psych, placeHolder, placeHolder, placeHolder, windEnsemble, bio, precalca),
-            new Student("Talia", placeHolder, csa, lang, placeHolder, windEnsemble, physics, calcBC)
+            new Student("Brynn", apush, bio, lang, forensics, windEnsemble, apSpanish, precalcA),
+            new Student("Tyler", psych, placeHolder, placeHolder, placeHolder, windEnsemble, bio, precalcA),
+            new Student("Talia", placeHolder, csa, lang, placeHolder, windEnsemble, physics, calcBC),
+            new Student("Wes", placeHolder, placeHolder, placeHolder, placeHolder, windEnsemble, placeHolder, precalcA)
         };
 
         for (Student s : classmates) message += ("You have " + Student.compareSchedules(me, s, blocks).size() + " classes with " + s.getName() + ": " + Student.compareSchedules(me, s, blocks) + "\n");
@@ -39,8 +40,11 @@ public class OpenSchedule
 
         ArrayList<Student> breakMates = Student.compareBreak(me, classmates, blocks);
         message += ("You have break with: ");
-        for (Student s : breakMates) message += (s + " ");
-        message += "\n"; 
+        for (int i = 0; i < breakMates.size(); i++) 
+        {
+            if (i == breakMates.size() - 1) message += ("and " + breakMates.get(i).getName() + "\n");
+            else message += (breakMates.get(i).getName() + ", ");
+        }
 
         Notify.showPopup("OpenSchedule", message);
         System.exit(0);
